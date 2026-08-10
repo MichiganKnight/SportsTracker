479573fe2a04385ab1fa93a125d466dc55faa6a3
Andrew Fleming <michiganknight2@gmail.com>
2026-08-10 18:44:21 -0500

    Minor Updates

---
+++ SportsTracker.Frontend/ViewModels/LeagueInfo/LeagueNavigationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SportsTracker.Shared.Enums;
+
+namespace SportsTracker.Frontend.ViewModels.LeagueInfo
+{
+    public sealed class LeagueNavigationViewModel
+    {
+        public League League { get; init; }
+        public LeaguePage ActivePage { get; init; }
+        
+        public string LeagueName { get; init; } = string.Empty;
+    }
+}
---
+++ SportsTracker.Shared/Enums/LeaguePage.cs
@@ -0,0 +1,8 @@
+﻿namespace SportsTracker.Shared.Enums
+{
+    public enum LeaguePage
+    {
+        Games,
+        Standings
+    }
+}
