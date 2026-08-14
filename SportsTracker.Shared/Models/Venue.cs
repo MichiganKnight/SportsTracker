27fff70bca0ce76aa2a213e61b052068d954fe25
Andrew Fleming <michiganknight2@gmail.com>
2026-08-13 20:35:43 -0500

    Updates

--- SportsTracker.Shared/Models/Venue.cs
+++ SportsTracker.Shared/Models/Venue.cs
@@ -5,9 +5,6 @@
         public string Id { get; init; } = string.Empty;
         public string Name { get; init; } = string.Empty;
         
-        public string? City { get; init; }
-        public string? State { get; init; }
-        
         public bool? IsIndoor { get; init; }
     }
 }
